00b34cf07e0084e3e15fbb71867274f1b96ed974
alvarolivie <alvarolivie@gmail.com>
2022-04-18 20:52:31 +0200

    vengaa

--- src/main/resources/static/js/Formulario.js
+++ src/main/resources/static/js/Formulario.js
@@ -1,44 +1,42 @@
+let rellenarFoto = (numArroz) => {
+    var panel=document.getElementById("fotoydescripcion");
+    if(numArroz==1){
+        let diseño='<div class="row">\n' +
+            '                <img src="resources/estrella.jpg" width="80%" height="80%">\n' +
+            '            </div>\n' +
+            '            <div class="row">\n' +
+            '                <p class="lead text-muted">Descripcion del arroz aqui ..............................</p>\n' +
+            '            </div>';
+        panel.innerHTML=diseño;
+    }
+    if(numArroz==2){
+        let diseño='<div class="row">\n' +
+            '                <img src="resources/alamar.jpg" width="80%" height="80%">\n' +
+            '            </div>\n' +
+            '            <div class="row">\n' +
+            '                <p class="lead text-muted">Descripcion del arroz aqui ..............................</p>\n' +
+            '            </div>';
+        panel.innerHTML=diseño;
+
+    }
+    if(numArroz==3){
+        let diseño='<div class="row">\n' +
+            '                <img src="resources/conejo.jpg" width="80%" height="80%">\n' +
+            '            </div>\n' +
+            '            <div class="row">\n' +
+            '                <p class="lead text-muted">Descripcion del arroz aqui ..............................</p>\n' +
+            '            </div>';
+        panel.innerHTML=diseño;
+
+    }
+}
+
+let mandarForm = () => {
+
+}
 
 
-function selectPedido(){
-    let numArroz;
-    console.log(num);
-    numArroz=num;
-    console.log(numArroz);
-    window.location.assign("./Pedido2.html");
-    window.addEventListener('load',function() {
-        console.log(numArroz);
-        var panel=document.getElementById("fotoydescripcion");
-        if(numArroz==1){
-            let diseño='<div class="row">\n' +
-                '                <img src="resources/estrella.jpg" width="80%" height="80%">\n' +
-                '            </div>\n' +
-                '            <div class="row">\n' +
-                '                <p class="lead text-muted">Descripcion del arroz aqui ..............................</p>\n' +
-                '            </div>';
-            panel.innerHTML=diseño;
-        }
-        if(numArroz==2){
-            let diseño='<div class="row">\n' +
-                '                <img src="resources/alamar.jpg" width="80%" height="80%">\n' +
-                '            </div>\n' +
-                '            <div class="row">\n' +
-                '                <p class="lead text-muted">Descripcion del arroz aqui ..............................</p>\n' +
-                '            </div>';
-            panel.innerHTML=diseño;
-
-        }
-        if(numArroz==3){
-            let diseño='<div class="row">\n' +
-                '                <img src="resources/conejo.jpg" width="80%" height="80%">\n' +
-                '            </div>\n' +
-                '            <div class="row">\n' +
-                '                <p class="lead text-muted">Descripcion del arroz aqui ..............................</p>\n' +
-                '            </div>';
-            panel.innerHTML=diseño;
-
-        }
-    }, true);
-
-
+let selectPedido = () => {
+    let numArroz = location.search.substring(1);
+    rellenarFoto(numArroz);
 }
