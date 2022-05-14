e0eb8d22f4606098e47c93b7d5c2fc9b641aaf5a
aleedelarica <delaricalejandra@gmail.com>
2022-05-14 17:52:10 +0200

    frontend owner

--- src/main/resources/static/js/Gestor.js
+++ src/main/resources/static/js/Gestor.js
@@ -2,7 +2,7 @@ let pedidos;
 
 let cargarPedidos=async()=>{
     //let roles = sessionStorage["roles"];
-    let roles = [1,2];
+    let roles = [1,2,3];
     let res=await fetch("elPeroli/v1/owner/viewPedidos");
     console.log(res);
     if (res.ok) {
@@ -46,6 +46,34 @@ let cargarPedidos=async()=>{
 
             }
         }
+    }else if(roles.length==3){
+
+        let tabla = document.getElementById('pedidos');
+        for(let i=0; i <pedidos.length; i++){
+
+            let newRow = tabla.insertRow(tabla.length);
+            let pedidoActual=json2array(pedidos[i]);
+            for(let j=0; j<pedidoActual.length; j++){
+                let cell = newRow.insertCell(j);
+                if(j==11){
+                    if(pedidoActual[j]=="ACEPTADA"){
+                        cell.innerHTML= '<input type="radio" id="aceptado'+pedidoActual[0]+'" name="pedido'+pedidoActual[0]+'" value="1" checked>' + '<input type="radio" id="rechazado'+pedidoActual[0]+'" name="pedido'+pedidoActual[0]+'" value="2">';
+                    }else if(pedidoActual[j]=="RECHAZADA"){
+                        cell.innerHTML= '<input type="radio" id="aceptado'+pedidoActual[0]+'" name="pedido'+pedidoActual[0]+'" value="1">' + '<input type="radio" id="rechazado'+pedidoActual[0]+'" name="pedido'+pedidoActual[0]+'" value="2" checked>'
+                    }else if(pedidoActual[j]=="PENDIENTE"){
+                        cell.innerHTML= '<input type="radio" id="aceptado'+pedidoActual[0]+'" name="pedido'+pedidoActual[0]+'" value="1">' + '<input type="radio" id="rechazado'+pedidoActual[0]+'" name="pedido'+pedidoActual[0]+'" value="2">'
+                    }
+
+                }else if(j==10){
+                    cell.innerHTML = '<input type="number" id="numPersonas' + pedidoActual[0] + '" name="numPersonas" value="'+pedidoActual[j]+'"><button type="button" class="btn btn-secondary" id="actualizarnumPersonas' + pedidoActual[0] + '" name="actualizarnumPersonas">Actualizar</button>';
+                }else if(j==4){
+                cell.innerHTML = '<input type="text" id="precio' + pedidoActual[0] + '" name="precio" value="'+pedidoActual[j]+'"><button type="button" class="btn btn-secondary" id="actualizarprecio' + pedidoActual[0] + '" name="actualizarprecio">Actualizar</button>';
+            }else{
+                    cell.innerHTML = pedidoActual[j];
+                }
+
+            }
+        }
     }
 
 }
@@ -76,5 +104,7 @@ let buscaTabla=()=>{
         row=table.rows[r++];
     }
 }
+
+
 
 
