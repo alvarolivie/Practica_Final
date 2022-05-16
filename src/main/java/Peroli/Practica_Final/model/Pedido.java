87e44dc66fa60a678e0ed164af15ba8911e775c3
aleedelarica <delaricalejandra@gmail.com>
2022-05-16 20:18:31 +0200

    security

--- src/main/java/Peroli/Practica_Final/model/Pedido.java
+++ src/main/java/Peroli/Practica_Final/model/Pedido.java
@@ -9,9 +9,11 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Embedded;
 import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.lang.Nullable;
 
-import javax.validation.constraints.Email;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -23,15 +25,34 @@ public class Pedido {
 
     @Id
     private Long id;
+
+    @Email
     private String email;
+
+    @Size(min=3,max=30)
     private String fullname;
+
+    @Size(min=9,max=12)
     private String tel;
+
+    @Min(5)
+    @Max(50)
     private int personas;
+
+
     private int arroz;
+
+    @DateTimeFormat(pattern="mm/dd/yyyy")
+    @Future
     private LocalDate fecha;
+
     private Momento momento;
+
     private String ciudad;
+
+    @Size(min=5,max=40)
     private String dir1;
+
     private BigDecimal precio = BigDecimal.valueOf(0);
     private Estado estado = Estado.PENDIENTE;
 
---
+++ src/test/java/Peroli/Practica_Final/model/PedidoTest.java
@@ -0,0 +1,8 @@
+package Peroli.Practica_Final.model;
+
+import org.junit.jupiter.api.Test;
+
+public class PedidoTest {
+
+
+}
